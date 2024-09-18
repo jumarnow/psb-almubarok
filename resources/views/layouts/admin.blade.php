247c92101293f7ea73f6189fba8669a8a87e81d3
jumarnow <jumarno.mwb@gmail.com>
2024-09-18 13:08:11 +0700

    url sidebar

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -77,7 +77,7 @@
                         </a>
                         <div class="dropdown-menu dropdown-menu-right">
 
-                            <a href="{{ url('/') }}" class="dropdown-item has-icon">
+                            <a href="/" class="dropdown-item has-icon">
                                 <i class="fas fa-home"></i> Landing Page
                             </a>
                             <a href="#" class="dropdown-item has-icon">
@@ -108,15 +108,15 @@
                     <ul class="sidebar-menu">
                         <li class="menu-header">Main Menu</li>
                         <li class="{{ request()->is('admin') ? 'active' : '' }}"><a class="nav-link"
-                                href="{{ url('admin') }}"><i class="fas fa-home fa-fw"></i>
+                                href="admin"><i class="fas fa-home fa-fw"></i>
                                 <span>Dashboard</span></a></li>
 
                         <li class="{{ request()->is('admin/formulir') ? 'active' : '' }}"><a class="nav-link"
-                                href="{{ url('admin/formulir') }}"><i class="fas fa-file-archive    "></i>
+                                href="admin/formulir"><i class="fas fa-file-archive    "></i>
                                 <span>Formulir</span></a></li>
 
                         <li class="{{ request()->is('admin/pengumuman') ? 'active' : '' }}"><a class="nav-link"
-                                href="{{ url('admin/pengumuman') }}"><i class="fas fa-bullhorn fa-fw"></i>
+                                href="admin/pengumuman"><i class="fas fa-bullhorn fa-fw"></i>
                                 <span>Pengumuman</span></a></li>
                     </ul>
                 </aside>
